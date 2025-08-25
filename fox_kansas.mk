50ddad54c74b0b92dd93179c939b5577f9ff7095
DUptain1993 <daniel.r.uptain93@gmail.com>
2025-08-25 12:50:46 -0700

    Create fox_kansas.mk

---
+++ fox_kansas.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from kansas device
+$(call inherit-product, device/motorola/kansas/device.mk)
+
+PRODUCT_DEVICE := kansas
+PRODUCT_NAME := fox_kansas
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g - 2025
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp
+
+# Inherit OrangeFox
+$(call inherit-product, vendor/omni/config/common.mk)
